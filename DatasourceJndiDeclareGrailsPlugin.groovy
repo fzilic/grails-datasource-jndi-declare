2c6d6f8f15ac97df8b468eb9f29cca9e842fb4df
Burt Beckwith <burt@burtbeckwith.com>
2013-11-13 11:41:36 -0500

    cleanup

--- DatasourceJndiDeclareGrailsPlugin.groovy
+++ DatasourceJndiDeclareGrailsPlugin.groovy
@@ -3,10 +3,6 @@ import org.grails.plugins.datasourcejndideclare.UpdateWebDescriptor
 class DatasourceJndiDeclareGrailsPlugin {
   def version = '1.0.0'
   def grailsVersion = '2.0 > *'
-  def dependsOn = [:]
-  def pluginExcludes = [
-    'grails-app/**'
-  ]
 
   def title = 'Datasource JNDI declare'
   def author = 'Franjo Žilić'
@@ -19,24 +15,14 @@ is probably what you want if your''re using JNDI datasource.
 
 Credits for original in-house plugin go to Zoran Regvart.'''
 
-  // URL to the plugin's documentation
   def documentation = 'https://github.com/fzilic/grails-datasource-jndi-declare/blob/master/README.md'
 
-  // License: one of 'APACHE', 'GPL2', 'GPL3'
   def license = "APACHE"
-
-  // Details of company behind the plugin (if there is one)
   def organization = [name: "CROZ d.o.o.", url: "http://www.croz.net/"]
-
-  // Any additional developers beyond the author specified above.
   def developers = [
     [name: "Zoran Regvart", email: "zregvart@croz.net"]
   ]
-
-  // Location of the plugin's issue tracker.
   def issueManagement = [ system: "github", url: "https://github.com/fzilic/grails-datasource-jndi-declare/issues" ]
-
-  // Online location of the plugin's browseable source code.
   def scm = [ url: 'https://github.com/fzilic/grails-datasource-jndi-declare' ]
 
   def doWithWebDescriptor = { xml ->
