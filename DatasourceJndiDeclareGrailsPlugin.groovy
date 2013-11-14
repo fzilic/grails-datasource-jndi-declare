fee2d1caa60009fcaa60808a1f4c48ae935685ef
Franjo Žilić <frenky666@gmail.com>
2013-11-14 10:49:47 +0100

    restored plugin descriptor comments for developer reference

--- DatasourceJndiDeclareGrailsPlugin.groovy
+++ DatasourceJndiDeclareGrailsPlugin.groovy
@@ -15,14 +15,24 @@ is probably what you want if your''re using JNDI datasource.
 
 Credits for original in-house plugin go to Zoran Regvart.'''
 
+  // URL to the plugin's documentation
   def documentation = 'https://github.com/fzilic/grails-datasource-jndi-declare/blob/master/README.md'
 
+  // License: one of 'APACHE', 'GPL2', 'GPL3'
   def license = "APACHE"
+
+  // Details of company behind the plugin (if there is one)
   def organization = [name: "CROZ d.o.o.", url: "http://www.croz.net/"]
+
+  // Any additional developers beyond the author specified above.
   def developers = [
     [name: "Zoran Regvart", email: "zregvart@croz.net"]
   ]
+
+  // Location of the plugin's issue tracker.
   def issueManagement = [ system: "github", url: "https://github.com/fzilic/grails-datasource-jndi-declare/issues" ]
+
+  // Online location of the plugin's browseable source code.
   def scm = [ url: 'https://github.com/fzilic/grails-datasource-jndi-declare' ]
 
   def doWithWebDescriptor = { xml ->
